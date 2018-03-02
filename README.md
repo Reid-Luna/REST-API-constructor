bf8c46fa8a9c6dc0ef74f8095799300b1c811e18
reid luna <reidluna@icloud.com>
2018-03-01 17:05:40 -0700

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# api.constructor
-a helpful constructor for REST APIs
+# REST-API-constructor
+### a helpful constructor for REST APIs using Express, CORS, body-parser, and Mongoose.
+
+## to install the constructor use :
+```
+npm install --save REST-API-constructor
+```
+
+## example usage :
+```js
+const API = require('REST-API-constructor');
+let test = new API(/* see route name */, /* see schema */, /* see port */);
+```
+
+## route name :
+### the API constructor will create 5 routes with the route name. :
+```js
+app.get('/test', ...)
+app.get('/test/:id', ...)
+app.post('/test', ...)
+app.put('/test/:id', ...)
+app.delete('/test/:id', ...)
+```
+
+## schema :
+### this is just a basic schema, the constructor will convert it into a mongoose schema. :
+```js
+let schema = {
+	fname: String,
+	lname: String
+}
+```
+
+## port:
+### this will run the api on the port specified
---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const app = express()
+
+
+const cors = require('cors')
+app.use(cors())
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const mongoose = require('mongoose')
+const db = mongoose.connect('mongodb://localhost:27017/test-one')
+
+
+class API {
+
+	constructor(model, oldschema, port) {
+
+		app.get(`/${model}`, (req, res) => {
+			this[model+'get'](res)
+		});
+
+		app.get(`/${model}/:id`, (req, res) => {
+			this[model+'getById'](req, res)
+		})
+
+		app.post(`/${model}`, (req, res) => {
+			this[model+'post'](req, res)
+		});
+
+		app.put(`/${model}/:id`, (req, res) => {
+			this[model+'put'](req, res)
+		});
+
+		app.delete(`/${model}/:id`, (req, res) => {
+			this[model+'delete'](req, res)
+		});
+
+
+		let schema = new mongoose.Schema(oldschema)
+		this[model] = mongoose.model(model, schema)
+
+
+		this[model+'get'] = (res) => {
+			this[model].find({}, (err, result) => {
+				if (err){
+					res.send(err)
+				} else {
+					res.json(result)
+				}
+			})
+		}
+
+		this[model+'getById'] = (req, res) => {
+			let id = req.params.id
+			this[model].findById(id, (err, result) => {
+				if (err) {
+					res.send(err)
+				} else {
+					res.json(result)
+				}
+			})
+		}
+
+		this[model+'post'] = (req, res) => {
+			this[model].create(req.body, (err, result) => {
+				if (err) {
+					res.send(err)
+				} else {
+					res.json(result)
+				}
+			})
+		}
+
+		this[model+'put'] = (req, res) => {
+			let toSet = req.body.toSet
+			let data = req.body.data
+			let id = req.params.id
+			let query = {
+				[toSet]: data
+			}
+			this[model].findByIdAndUpdate(id, query, {new:true}, (err, result) => {
+				if (err) {
+					res.send(err)
+				} else {
+					res.json(result)
+				}
+			})
+		}
+
+		this[model+'delete'] = (req, res) => {
+			let id = req.params.id
+			this[model].findByIdAndRemove(id, (err, result) => {
+				if (err) {
+					res.send(err)
+				} else {
+					res.json(result)
+				}
+			})
+		}
+
+	}
+}
+
+app.listen(port, (err) => {
+	if (err){
+		console.err
+	} else {
+		console.log(`${model} API running on port ${port}`)
+	}
+})
+
+module.exports = API
